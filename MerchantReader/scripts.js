52d6728b936a62f1384bcf3920e5a31fee97dc10
Gregorius Moses Marevson <13520052@std.stei.itb.ac.id>
2024-04-05 20:36:17 +0700

    fix pesan transaksi

--- MerchantReader/scripts.js
+++ MerchantReader/scripts.js
@@ -1,5 +1,5 @@
-var api_key = "4HUPWPTBRAS5HQ0C";
-var url = "https://api.thingspeak.com/channels/2497964/fields/1.json?api_key=" + api_key + "&results=1";
+var api_key = "YJMP9RMTL8LL3KKS";
+var url = "https://api.thingspeak.com/channels/2498284/feeds.json?api_key=" + api_key + "&results=2";
 
 function httpGetAsync(url, callback)
 {
@@ -13,8 +13,16 @@ function httpGetAsync(url, callback)
 }
 
 var saldo = document.getElementById("saldo");
+var pesan_transaksi = document.getElementById("pesan_transaksi")
 setInterval(() => {
     httpGetAsync(url, (response) => {
-        saldo.innerText = JSON.parse(response)["feeds"][0]["field1"];
+        let saldoValue = JSON.parse(response)["feeds"][0]["field1"];
+        if (JSON.parse(response)["feeds"][1]["field2"] == 1) {
+            pesan_transaksi.innerText = "SALDO AWAL: " + saldoValue;    
+        } else if (JSON.parse(response)["feeds"][1]["field2"] == 2) {
+            pesan_transaksi.innerText = "TRANSAKSI BERHASIL, SISA SALDO " + saldoValue;
+        } else if (JSON.parse(response)["feeds"][1]["field2"] == 3){
+            pesan_transaksi.innerText = "SALDO TIDAK MENCUKUPI";
+        }
     });
 }, 1000);
--- UTS_IoT/UTS_IoT.ino
+++ UTS_IoT/UTS_IoT.ino
@@ -1,18 +1,24 @@
 #include <WiFi.h>
 #include "ThingSpeak.h"
 
+const int INIT = 1;
+const int SUCCESS = 2;
+const int FAILED = 3;
+
 const int gpio = 4;
 const int led = 2;
 
-const char *writeAPIKey = "JZ51OBBLGM3TCERF";
-const char *readAPIKey = "4HUPWPTBRAS5HQ0C";
+const char *writeAPIKey = "548GBNJSVYAIRURX";
+const char *readAPIKey = "YJMP9RMTL8LL3KKS";
 const char *ssid = "Tubagus ismail lt 14G";
 const char *pass = "444444444";
 WiFiClient client;
-unsigned long channelID = 1;
+unsigned long channelID = 2;
 
 int state = 0;
 int money = (52 + 100) * 1000; // nim: 052, saldo awal: 152.000
+int field1Status = 0;
+int field2Status = 0;
 
 void connectWifi() {
   Serial.print("Connecting to ");
@@ -38,23 +44,35 @@ void setup() {
   connectWifi();
   ThingSpeak.begin(client);
 
-  while (ThingSpeak.writeField(channelID, 1, money, writeAPIKey) != 200) {
-    Serial.println("Failed sending initial money");
-  }
+  do {
+    field1Status = ThingSpeak.writeField(channelID, 1, money, writeAPIKey);
+  } while (field1Status != 200);
+  do {
+    field2Status = ThingSpeak.writeField(channelID, 2, INIT, writeAPIKey);
+  } while (field2Status != 200);
 }
 
 void loop() {
   state = touchRead(gpio);
+  field1Status = 0;
+  field2Status = 0;
 
   if (state <= 30) { // check if GPIO4 is touched
     if (isMoneySufficient()) {
-      while (ThingSpeak.writeField(channelID, 1, money - 20000, writeAPIKey) != 200) {
-        Serial.println("Failed sending current money");
-      }
       money -= 20000;
+      do {
+        field1Status = ThingSpeak.writeField(channelID, 1, money, writeAPIKey);
+      } while (field1Status != 200);
+      do {
+        field2Status = ThingSpeak.writeField(channelID, 2, SUCCESS, writeAPIKey);
+      } while (field2Status != 200);
       digitalWrite(led, HIGH);
       delay(5000);
     } else {
+      do {
+        // field1Status = ThingSpeak.writeField(channelID, 1, money, writeAPIKey);
+        field2Status = ThingSpeak.writeField(channelID, 2, FAILED, writeAPIKey);
+      } while (field2Status != 200);
       for (int i = 0; i < 5; i++) {
         digitalWrite(led, HIGH);
         delay(500);
